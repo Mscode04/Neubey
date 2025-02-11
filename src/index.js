b48fc3b517f893cf8c67ede13588d7938ee328ec
Mscode04 <ms.shaheenkp@gmail.com>
2025-02-11 06:43:27 +0530

    app-updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import Main from './Main';
 import { register } from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename="/Neubey"> {/* Only one BrowserRouter */}
+    <Router> {/* Remove basename */}
       <Main />
     </Router>
   </React.StrictMode>
